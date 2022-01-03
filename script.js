fe2d749012351deb447de1cba9f20fb4a8b38399
Acsa Cibely <86966197+acsacibely@users.noreply.github.com>
2022-01-03 12:59:06 -0300

    about-edit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
  
-//filtro do poemas
+//FILTRO DOS POEMAS
 var bts = document.getElementsByClassName("filter-btn");
 for(bt of bts){
     adEvent(bt.id);
 }
+/*ADICIONA EVENTO DE CLICK NOS BOTÕES DOS CARDS
+
+DESSA FORMA, AO CLICAR NO NOME DOS AUTORES INDIVIDUALMENTE
+
+A ANIMAÇÃO FILTRARÁ APENAS OS POEMAS DO AUTOR ESCOLHIDO
+
+E ESCONDERÁ OS OUTROS POEMAS
+*/
 function adEvent(bt){
     let botao = document.getElementById(bt);
     botao.addEventListener("click", () => {
@@ -29,7 +37,7 @@ function adEvent(bt){
     });
 }
 
-//animação da máquina de escrever
+//ANIMAÇÃO MÁQUINA DE ESCREVER NO TÍTULO
 function typeWriter(elemento) {
     const textArray = elemento.innerHTML.split('');
     elemento.innerHTML= '';
@@ -40,3 +48,15 @@ function typeWriter(elemento) {
 
 const titulo = document.getElementById('title');
 typeWriter(titulo);
+
+/*
+
+COM ESSA FUNÇÃO, O TÍTULO DO SITE NO ARQUIVO index.html TEM A ANIMAÇÃO DE MÁQUINA DE ESCREVER
+
+PARA ISSO, AS LETRAS DO TÍTULO FORAM SEPARADAS COM .split E FOI USADA A FUNÇÃO forEach, OU SEJA,
+
+PARA CADA LETRA, DEVE-SE AGUARDAR UM TEMPO PARA APARECER, TEMPO ESSE QUE FOI ESTIPULADO COM O 
+
+setTimemout()
+
+*/
